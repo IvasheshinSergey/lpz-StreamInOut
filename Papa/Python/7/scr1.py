2e0a5d5fd1d0b3238610f50ac23e51347f1a1c69
IvasheshinSergey <115476514+IvasheshinSergey@users.noreply.github.com>
2022-11-27 14:15:51 +0600

    Add files via upload

---
+++ Papa/Python/7/scr1.py
@@ -0,0 +1,2 @@
+print('Hello world for the alliance!!!')
+pritn('Hello world for the alliance!!!')
---
+++ Papa/Python/8/CS/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+
+            Console.WriteLine("This is CSharp");    
+            Console.WriteLine(s);
+            Console.WriteLine("This is CSharp");
+        }
+    }
+}
